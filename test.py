5d29f36796e777eb7ba58fe80e22031336c66853
kinga5 <kinga112@yahoo.com>
2019-02-28 18:09:46 -0500

    Initial commit

---
+++ test.py
@@ -0,0 +1,12 @@
+#import tensorflow as tf
+import numpy as np
+
+
+
+#def main():
+#	hello = tf.constant('hello TF')
+#	sess = tf.Session()
+#	print(sess.run(hello))
+#
+#if __name__ == '__main__':
+#    main()
